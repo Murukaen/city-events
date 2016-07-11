fd4efd0878ce9cfb39c278c14d3e44ddeaf9feab
razvan <razzvan.savu@gmail.com>
2016-07-11 12:57:31 +0300

    Add user roles

--- imports/ui/pages/register.js
+++ imports/ui/pages/register.js
@@ -4,13 +4,17 @@ Template.register.events({
     'submit form': function(event) {
         event.preventDefault();
         Accounts.createUser({
-            'email': event.target.email.value,
-            'password': event.target.password.value
+            email: event.target.email.value,
+            password: event.target.password.value,
+            profile: {
+                isOrganizer: $("[name='organizer']").is(":checked")
+            }
         }, function(error) {
             if (error) {
                 console.log(error.reason);
             }
             else {
+                //Roles.addUsersToRoles(Meteor.userId(), event.target.role.value);
                 Router.go('home');
             }
         });
