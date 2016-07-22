b755484ad25b796ada8422e76d81814ac188e34a
razvan <razzvan.savu@gmail.com>
2016-07-22 10:15:28 +0300

    Add date validation. Display map after location input.

--- imports/ui/pages/add-event.js
+++ imports/ui/pages/add-event.js
@@ -17,30 +17,82 @@ function displayImage(imgObj) {
     });       
 }
 
-Template.addEvent.onRendered(function() {
-        $('.datetimepicker').datetimepicker({
-            defaultDate: new Date(),
-            format: "DD-MM-YYYY HH:mm"
-        });
-        GoogleMaps.load({
-            key: 'AIzaSyBY0lDEE8uNafAydB8sJlj3SMKZQ1rr4Ls',
-            libraries: 'places'
-        });
-        this.autorun(function () {
-            if (GoogleMaps.loaded()) {
-                $("#inputLocation").geocomplete({
-                    map: '#map'
-                });
+function initValidator() {
+    var validator = $('.event-setup').validate({
+        rules: {
+            name: {
+                maxlength: 20
             }
+        },
+        submitHandler: function(event) {
+            Meteor.call('addEvent', event.target.name.value, event.target.location.value, event.target.date.value, $('#imgDropZone').attr('imgUrl'), function () {
+                Router.go('home');
+            });
+        }
+    });
+}
+
+function initDateTimePickers() {
+    $('#start-date')
+        .datetimepicker({
+            defaultDate: new Date(),
+            format: "DD-MM-YYYY HH:mm",
+            minDate: new Date()
+        })
+        .on("dp.change", () => {
+            var startDate = $('#start-date').data('DateTimePicker').date()
+            $("#end-date").data('DateTimePicker').minDate(startDate);
         });
+    $('#end-date')
+        .datetimepicker({
+            defaultDate: new Date(),
+            format: "DD-MM-YYYY HH:mm",
+        })
+        .on("dp.change", () => {
+            var endDate = $('#end-date').data('DateTimePicker').date()
+            $("#start-date").data('DateTimePicker').maxDate(endDate);
+        });
+}
+
+function initGoogleMaps() {
+    GoogleMaps.load({
+        key: 'AIzaSyBY0lDEE8uNafAydB8sJlj3SMKZQ1rr4Ls',
+        libraries: 'places'
+    });
+    Tracker.autorun(function () {
+        if (GoogleMaps.loaded()) {
+            $("#inputLocation")
+                .geocomplete({
+                    map: '#map'//,
+                    // markerOptions: {
+                    //     draggable: true
+                    // }
+                })
+                .bind("geocode:result", function(event, result){
+                    $('#map').show();
+                    var map = $("#inputLocation").geocomplete("map");
+                    google.maps.event.trigger(map, 'resize');
+                    console.log(result.geometry.viewport);
+                    map.panTo(result.geometry.location);
+                    // map.setZoom(12);
+                    // map.fitBounds(result.geometry.bounds);
+                })
+                .bind("geocode:error", function(event, result){
+                    console.log("Geo error:", result);
+                });
+        }
+    });
+}
+
+Template.addEvent.onRendered(function() {
+    initValidator();
+    initGoogleMaps();
+    initDateTimePickers();
 });
 
 Template.addEvent.events({
     'submit form': function(event) {
         event.preventDefault();
-        Meteor.call('addEvent', event.target.name.value, event.target.location.value, event.target.date.value, $('#imgDropZone').attr('imgUrl'), function () {
-            Router.go('home');
-        });
     },
     'dropped #imgDropZone': function(event) {
         FS.Utility.eachFile(event, function(file) {
