9e48f6ecf2a39b0cdf950151033f8d6fa9a9821f
razvan <razzvan.savu@gmail.com>
2016-08-01 18:11:12 +0300

    Add events collection schema

--- imports/api/events/events.js
+++ imports/api/events/events.js
@@ -1,1 +1,37 @@
 Events = new Meteor.Collection('events'); // TODO export const
+
+EventSchema = new SimpleSchema({
+    name: {
+        label: 'Event name',
+        type: String
+    },
+    location: {
+        label: 'Event location',
+        type: String
+    },
+    startDate: {
+        label: 'Event start datetime',
+        type: String
+    },
+    endDate: {
+        label: 'Event end datetime',
+        type: String
+    },
+    imgUrl: {
+        label: 'Event image url',
+        type: String,
+        regEx: SimpleSchema.RegEx.Url,
+        optional: true
+    },
+    labels: {
+        label: 'Event labels',
+        type: [String],
+        optional: true
+    },
+    createdBy: {
+        label: 'Event organizer',
+        type: String
+    }
+});
+
+Events.attachSchema(EventSchema);
