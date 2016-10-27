cfb6d22c016e8b7e8f45179cf677ecce5ddda44c
razvan <razzvan.savu@gmail.com>
2016-10-27 11:07:55 +0300

    edit-event: add condition: current_date < start_date < end_date

--- imports/ui/pages/edit-event.js
+++ imports/ui/pages/edit-event.js
@@ -3,20 +3,12 @@ import './edit-event.html';
 function initDateTimePickers() {
     $('[name=endDate]').data('DateTimePicker').setMinDate($('[name=startDate]').data('DateTimePicker').date);
     $('[name=startDate]').data('DateTimePicker').setMaxDate($('[name=endDate]').data('DateTimePicker').date);
+    $('[name=startDate]').data('DateTimePicker').setMinDate(moment().toDate())
     $('[name=startDate]')
-        .datetimepicker({
-            defaultDate: moment(),
-            format: "DD-MM-YYYY HH:mm",
-            minDate: moment()
-        })
         .on("dp.change", () => {
             $('[name=endDate]').data('DateTimePicker').setMinDate($('[name=startDate]').data('DateTimePicker').date);
         });
     $('[name=endDate]')
-        .datetimepicker({
-            defaultDate: moment(),
-            format: "DD-MM-YYYY HH:mm",
-        })
         .on("dp.change", () => {
             $('[name=startDate]').data('DateTimePicker').setMaxDate($('[name=endDate]').data('DateTimePicker').date);
         });
@@ -33,7 +25,6 @@ Template.editEvent.onRendered( () => {
 /* EVENTS */
 Template.editEvent.events({
     'submit form': function(event) {
-        // console.log()
         Router.go('myEvents');
     }
 });
