c347e16433ac6849f103cc63ef30d9ed8536276c
razvan <razzvan.savu@gmail.com>
2016-11-05 18:09:49 +0200

    Remove google-places feature

--- imports/ui/pages/add-event.js
+++ imports/ui/pages/add-event.js
@@ -48,36 +48,6 @@ function initDateTimePickers() {
         });
 }
 
-function initGoogleMaps() {
-    GoogleMaps.load({
-        key: 'AIzaSyBY0lDEE8uNafAydB8sJlj3SMKZQ1rr4Ls',
-        libraries: 'places'
-    });
-    Tracker.autorun(function () {
-        if (GoogleMaps.loaded()) {
-            $("#inputLocation")
-                .geocomplete({
-                    map: '#map'//,
-                    // markerOptions: {
-                    //     draggable: true
-                    // }
-                })
-                .bind("geocode:result", function(event, result){
-                    $('#map').show();
-                    var map = $("#inputLocation").geocomplete("map");
-                    google.maps.event.trigger(map, 'resize');
-                    console.log(result.geometry.viewport);
-                    map.panTo(result.geometry.location);
-                    // map.setZoom(12);
-                    // map.fitBounds(result.geometry.bounds);
-                })
-                .bind("geocode:error", function(event, result){
-                    console.log("Geo error:", result);
-                });
-        }
-    });
-}
-
 Template.addEvent.onCreated(() => {
     let template = Template.instance();
     template.labels = new ReactiveVar([]);
@@ -85,7 +55,6 @@ Template.addEvent.onCreated(() => {
 
 Template.addEvent.onRendered(function() {
     initValidator(Template.instance());
-    initGoogleMaps();
     initDateTimePickers();
 });
 
@@ -106,16 +75,6 @@ Template.addEvent.events({
 
 /* HELPERS */
 Template.addEvent.helpers({
-    mapOptions: function() {
-        // Make sure the maps API has loaded
-        if (GoogleMaps.loaded()) {
-          // Map initialization options
-          return {
-            center: new google.maps.LatLng(-37.8136, 144.9631),
-            zoom: 8
-          };
-        }
-    },
     labels() {
         return Template.instance().labels.get(); 
     }
