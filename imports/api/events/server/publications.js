58126e13f80e2aa3616266011b1fbc614e79ca9c
razvan <razzvan.savu@gmail.com>
2017-01-22 21:47:11 +0200

    Add filtering by date:current week

--- imports/api/events/server/publications.js
+++ imports/api/events/server/publications.js
@@ -1,18 +1,43 @@
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+function getStartOfCurrentWeek() {
+    let date = new Date();
+    let day = date.getDay() || 7;
+    if( day !== 1 ) 
+        date.setHours(-24 * (day - 1)); 
+    return date;
+}
+
+function setDateOnQuery(startDate, endDate, query) {
+    query.startDate = {$gte: startDate, $lte: endDate};
+}
+
 function CrietriaParser(criteria) {
     this.addLabel = function (query) {
         if (criteria.label) {
             query.labels = criteria.label;
         }
         if (criteria.date) {
+            let startDate = new Date();
+            let endDate = new Date();
             switch(criteria.date) {
                 case 'today': 
-                    let startDate = new Date();
                     startDate.setHours(0,0,0,0);
-                    let endDate = new Date();
                     endDate.setHours(23,59,59,999);
-                    query.startDate = {$gte: startDate, $lte: endDate};
+                    break;
+                case 'week':
+                    startDate = getStartOfCurrentWeek();
+                    endDate = addDays(startDate, 7);
+                    break;
+                case 'month':
+                    // TODO
                     break;
             }
+            setDateOnQuery(startDate, endDate, query);
         }
     }     
 }
