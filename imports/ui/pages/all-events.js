c522b708d1a243bbd2714ff86f92334f4d894948
razvan <razzvan.savu@gmail.com>
2017-01-24 13:46:16 +0200

    Display current filter label

--- imports/ui/pages/all-events.js
+++ imports/ui/pages/all-events.js
@@ -11,6 +11,12 @@ function filter(name, value) {
     Router.go('home', {}, {query: $.param(query)});
 }
 
+Template.allEvents.onCreated(function () {
+    let template = Template.instance();
+    let currentLabel = Session.get('query').label;
+    template.labels = new ReactiveVar(currentLabel ? [currentLabel] : []);
+});
+
 /* EVENTS */
 Template.allEvents.events({
     'submit #searchForm': function(event, template) {
@@ -32,5 +38,8 @@ Template.allEvents.helpers({
             events: Events.find({}).fetch(),
             clickRoute: 'view'
         }    
+    },
+    labels() {
+        return Template.instance().labels.get(); 
     }
 });
