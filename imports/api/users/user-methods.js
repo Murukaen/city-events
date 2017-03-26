6cb7537ef950dbe78ad5eea5d268052cf6bf3e73
razvan <razzvan.savu@gmail.com>
2017-03-26 12:27:12 +0300

    Link Fb account

--- imports/api/users/user-methods.js
+++ imports/api/users/user-methods.js
@@ -6,5 +6,10 @@ Meteor.methods({
               return Accounts.sendVerificationEmail( userId );
             }
         }
+    },
+    updateProfile({key, value}) {
+        if (Meteor.userId()) {
+            Meteor.users.update({_id: Meteor.userId()}, {$set: {["profile." + key]: value}});
+        }
     }
 });
--- imports/ui/pages/profile.js
+++ imports/ui/pages/profile.js
@@ -1,2 +1,15 @@
 import './profile.html'
 import './profile.css'
+
+Template.profile.events({
+    'click #fbLink': function() {
+        Meteor.linkWithFacebook((err) => {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                Meteor.user().profile.isLinkedWithFacebook = true;
+            }
+        });
+    }
+})
