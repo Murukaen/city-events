0361697e64db98fb740b7d1459aec9f07c8b2424
razvan <razvan@44tek.com>
2018-09-05 11:14:27 +0300

    [forgot-pass] Display msg on mail sent

--- imports/ui/pages/forgot-pass.js
+++ imports/ui/pages/forgot-pass.js
@@ -1,51 +1,56 @@
-import './forgot-pass.html';
+import './forgot-pass.html'
 
 var trimInput = function(val) {
-    return val.replace(/\s*/g, "");
+    return val.replace(/\s*/g, "")
 }
 
 var isNotEmpty = function(val) {
-    return val != null && val != "";
+    return val != null && val != ""
 }
 
 Template.forgotPassword.onCreated(() => {
-    let template = Template.instance();
-    template.errors = new ReactiveDict(); 
+    let template = Template.instance()
+    template.errors = new ReactiveDict()
+    template.mailSent = new ReactiveVar(false)
 })
 
 Template.forgotPassword.onRendered(function () {
-    let template = Template.instance();
+    let template = Template.instance()
     $('#forgotPasswordForm').validate({
         submitHandler() {
-            var email = trimInput($('#forgotPasswordForm #forgotPasswordEmail').val().toLowerCase());
+            var email = trimInput($('#forgotPasswordForm #forgotPasswordEmail').val().toLowerCase())
             Accounts.forgotPassword({email}, (err) => {
                 let errors = {}
                 if (err) {
                   if (err.message === 'User not found [403]') {
                     errors['email'] = 'This email does not exist.'
                   } else {
-                    console.log('We are sorry but something went wrong.', err);
+                    console.log('We are sorry but something went wrong.', err)
                   }
                 } else {
-                  console.log('Email Sent. Check your mailbox.');
+                  console.log('Email Sent. Check your mailbox.')
+                  template.mailSent.set(true)
                 }
                 template.errors.set(errors)
-            });
+            })
         }
-    });
-});
+    })
+})
 
 Template.forgotPassword.events({
   'submit #forgotPasswordForm': function(e, t) {
-    e.preventDefault();
+    e.preventDefault()
   },
   'keyup input[name="email"]': function(e,t) {
     t.errors.clear()
   } 
-});
+})
 
 Template.forgotPassword.helpers({
     error(name) {
-        return Template.instance().errors.get(name);
+        return Template.instance().errors.get(name)
+    },
+    mailSent() {
+        return Template.instance().mailSent.get()
     }
 })
