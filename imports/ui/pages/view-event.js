c092b9be630464ef99ed0293c3cdb942f1839c12
razvan <razvan@44tek.com>
2018-09-29 16:18:13 +0300

    [event-view] Add back button for mobile

--- imports/ui/pages/view-event.js
+++ imports/ui/pages/view-event.js
@@ -1,26 +1,29 @@
-import './view-event.html';
-import './view-event.css';
+import './view-event.html'
+import './view-event.css'
 
 Template.viewEvent.events({
-    'click #voteUp': function(event, template) {
+    'click #vote-up': function(event, template) {
         Meteor.call('voteUpEvent', template.data._id, (err) => {
             if (err) 
-                console.log(err);
-        });
+                console.log(err)
+        })
     },
-    'click #voteDown': function(event, template) {
+    'click #vote-down': function(event, template) {
         Meteor.call('voteDownEvent', template.data._id, (err) => {
             if (err) 
-                console.log(err);
-        });
+                console.log(err)
+        })
+    },
+    'click #go-back': function(event, template) {
+        history.back()
     }
-});
+})
 
 Template.viewEvent.helpers({
     hasVotedUp() {
-        return Template.instance().data.validatedBy.includes(Meteor.userId());
+        return Template.instance().data.validatedBy.includes(Meteor.userId())
     },
     hasVotedDown() {
-        return Template.instance().data.invalidatedBy.includes(Meteor.userId());
+        return Template.instance().data.invalidatedBy.includes(Meteor.userId())
     }
-});
+})
