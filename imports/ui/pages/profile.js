aad001b255b1f12459993e05d9c8e53e41135c44
razvan <razvan@44tek.com>
2018-10-04 21:28:34 +0300

    Beautify profile page. Add password change feature.

--- imports/ui/pages/profile.js
+++ imports/ui/pages/profile.js
@@ -1,6 +1,32 @@
 import './profile.html'
 import './profile.css'
 
+Template.profile.onRendered(function () {
+    $('.tab-content').css('display', 'none')
+    $(`.tab-content[name=${$('button.active').attr('name')}]`).css('display', 'block')
+    let validator = $('#change-pass').validate({
+        submitHandler() {
+            let oldPass = $('#old-pass').val()
+            let newPass = $('#new-pass').val()
+            Accounts.changePassword(oldPass, newPass, (err) => {
+                if (err) {
+                    if (err.reason == "Incorrect password") {
+                        validator.showErrors({
+                            old_password: err.reason    
+                        });
+                    } else {
+                        console.log(err)
+                    }
+                } else {
+                    $('#old-pass').val('')
+                    $('#new-pass').val('')
+                    sAlert.info("Password changed successfully")
+                }
+            })
+        }
+    })
+})
+
 Template.profile.events({
     'click #fbLink': function() {
         Meteor.linkWithFacebook((err) => {
@@ -11,6 +37,13 @@ Template.profile.events({
                 Meteor.call('updateProfile', {key: 'isLinkedWithFacebook', value: true});
             }
         });
+    },
+    'click .tabs button': function(e,t) {
+        $('.tab-content').css('display', 'none')
+        $('.tabs button').removeClass('active')
+        let name = $(e.target).attr('name')
+        $(`.tab-content[name=${name}]`).css('display', 'block')
+        $(`.tabs button[name=${name}]`).addClass('active')
     }
 })
 
